cd1844b1383f049c407310621def081e667cfaa8
Pranav Kamath <pranavkamath14@gmail.com>
2022-12-04 20:10:23 -0800

    Modified train.py. Model can now be trained.

--- train.py
+++ train.py
@@ -1,17 +1,40 @@
 from data_loader.loader import Dataset, LoadData
 from model.resnet_model import ResNetModel
+from torchvision import transforms
 
-def train_model(train_config_path):
-    
-    num_classes = None # TODO
-    train_loader, val_loader, test_loader = None, None, None # TODO
+def train_model(num_classes, train_loader, val_loader, test_loader):
+    print("Training Model...")
     model = ResNetModel(num_classes)
     model.train_model(train_config_path, train_loader, val_loader, test_loader)
     model.store_model()
     model.generate_model_backbone()
     model.store_model_backbone()
 
-if __name__ == "__main__":
+def create_dataset(csv_file_path, data_dir, transforms = None):
+    print("Creating Dataset...")
+    dataset = Dataset(csv_file_path, data_dir, transforms)
+    return dataset
 
+def create_dataloaders(dataset, json_path):
+    print("Creating Data Loaders...")
+    load_data = LoadData(dataset, json_path)
+    train_loader, val_loader, test_loader = load_data.get_data_loaders()
+    return train_loader, val_loader, test_loader
+
+if __name__ == "__main__":
+    #add data paths
     train_config_path = "train_config.json"
-    train_model(train_config_path)
+    csv_file_path = "data/fashion.csv"
+    data_dir = "data"
+
+    #transforms for image transformation
+    image_transforms = transforms.Compose([transforms.Resize((224, 224)),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize((0.5,), (0.5,))])
+
+    #create dataset
+    dataset = create_dataset(csv_file_path, data_dir, image_transforms)
+    #get number of classes
+    num_classes = dataset._get_num_classes()
+    train_loader, val_loader, test_loader = create_dataloaders(dataset, train_config_path)
+    train_model(num_classes, train_loader, val_loader, test_loader)
